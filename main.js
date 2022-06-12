c0b3dd39e4e49bce45b1aec14cdd644a3a3dd920
glopzel <74939915+lozzel@users.noreply.github.com>
2022-06-11 21:05:48 -0400

    changed fetch to async/await

--- main.js
+++ main.js
@@ -4,34 +4,30 @@ const pokeImage = document.querySelector('#pokeImg1');
 
 document.querySelector('input').addEventListener('change', cleanItUp);
 
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('button').addEventListener('click', getFetch);
 
-function getFetch(){
+async function fetchIt() {
   spanWords.innerText = '';
+
   const poke1 = document.querySelector('#poke1').value.toLowerCase()
 
-  const pokeFixed = poke1.split('').filter(item => item !== ' ').join(''); 
-  
-  const url = `https://pokeapi.co/api/v2/pokemon/${pokeFixed}`;
+  const pokeFixed = poke1.split('').filter(item => item !== ' ').join('');
 
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeFixed}`);
+  const data = await res.json();
 
-    let name = (data.name).toUpperCase();
-    let typeOfPokemon = data.types[0].type.name;
-    let abilityPokemon = data.abilities[0].ability.name;
+  let name = (data.name).toUpperCase();
+  let typeOfPokemon = data.types[0].type.name;
+  let abilityPokemon = data.abilities[0].ability.name;
 
-    pokemonName.innerText = name;
-    pokeImage.src = data.sprites.front_shiny;
+  pokemonName.innerText = name;
+  pokeImage.src = data.sprites.front_shiny;
 
-    spanWords.innerText = `This pokemon's type is ${typeOfPokemon} and has the ${abilityPokemon} ability`;
+  spanWords.innerText = `This pokemon's type is ${typeOfPokemon} and has the ${abilityPokemon} ability`;
     
-    spanWords.classList.add('textAnimation');
-  })
-  
-  .catch(error =>  console.log(`error ${err}`));
-};
+  spanWords.classList.add('textAnimation');
+
+}
 
 function cleanItUp() {
   pokemonName.innerText = '';
